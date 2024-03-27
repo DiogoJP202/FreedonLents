78dec3dd31009a8eb89c2706be0a1c085eecac0c
Diogo Antonny <Diogojp202@gmail.com>
2024-03-27 16:31:35 -0300

    Imagem 'Felipe' trocada, comentários adicionados no script, mediaQueries.css e mobile.css desenvolvivido e responsividade para versão desktop finalizada.

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,6 @@ window.onresize = () => location.reload();
 let blocoPreto = document.querySelectorAll(".fundoPreto");
 let sessaoAvaliacao = document.querySelectorAll(".avaliacao");
 
-blocoPreto[0].style.height = getComputedStyle(sessaoAvaliacao[0]).height;
+blocoPreto[0].style.height = getComputedStyle(sessaoAvaliacao[0]).height; // getComputedStyle captura todos os estilos de um elemento.
 blocoPreto[1].style.height = getComputedStyle(sessaoAvaliacao[1]).height;
 
